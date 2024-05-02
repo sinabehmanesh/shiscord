73432e0654dd67f5309775f4655eca63da6c6b70
sina.behmanesh <sina.behmanesh@agile-robots.com>
2024-05-02 16:46:10 +0200

    ADD

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module main
 go 1.22.2
 
 require (
+	github.com/MarkKremer/microphone v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gopxl/beep v1.4.1 // indirect
+	github.com/gordonklaus/portaudio v0.0.0-20230709114228-aafa478834f5 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/gorilla/sessions v1.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,5 +1,61 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+
+	"github.com/MarkKremer/microphone"
+	"github.com/gopxl/beep/wav"
+	"github.com/gordonklaus/portaudio"
+)
+
 func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Feed the device.")
+		return
+	}
+
+	fmt.Println("Recording. Press Ctrl-C to stop.")
+
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+
+	stream, format, err := microphone.OpenDefaultStream(44100, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filename := os.Args[1]
+
+	if !strings.HasSuffix(filename, ".wav") {
+		filename += ".wav"
+	}
+
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+
+	go func() {
+		<-sig
+		stream.Stop()
+		stream.Close()
+	}()
+
+	stream.Start()
+	defer stream.Close()
+
+	var ti []byte
+	go wav.Encode(ti, stream, format)
+
+	time.Sleep(10 * time.Second)
 
 }
